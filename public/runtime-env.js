ff25835a9fa734bb8bebb3227d85d3e4358a8bc9
pepperrkimm <pepperrkimm@gmail.com>
2025-03-13 13:44:28 +0900

    chore: .gitignore 파일 생성

--- public/runtime-env.js
+++
@@ -1,6 +0,0 @@
-window.__runtime_config__ = {
-    AUTH_URL: 'http://20.1.2.3/api/auth',
-    BILLING_URL: 'http://20.1.2.3/api/billings',
-    PRODUCT_URL: 'http://20.1.2.3/api/products',
-    CUSTOMER_URL: 'http://20.1.2.3/api/customers'
-  };
